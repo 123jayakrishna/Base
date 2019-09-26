35ffee2218514d2dd7540c3aca8e6cf47dbc49c7
123jayakrishna <jayakrishna.n@adeptpros.com>
2019-09-26 17:54:13 +0530

    Added Syuident page and Student Constants.

---
+++ src/test/java/Pages/StudentPage.java
@@ -0,0 +1,31 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utils.BaseClassForDM;
+import utils.Constants;
+
+public class StudentPage extends BaseClassForDM {
+
+    public StudentPage(WebDriver driver)
+    {
+        super(driver);
+    }
+
+    @FindBy(id = "txbStudentId")
+    private WebElement txt_StudentId;
+
+    @FindBy(id = "txbSessionCode1")
+    private WebElement txt_SessionCode;
+
+    @FindBy(id = "btnLogin1")
+    private WebElement btn_Login;
+
+    public void logIntoStudentPage()
+    {
+        txt_StudentId.sendKeys(Constants.STUDENT_ID);
+        txt_SessionCode.sendKeys(Constants.SESSION_ID);
+        btn_Login.click();
+    }
+}
--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -10,6 +10,9 @@ public final class Constants {
     public static final String PROCTOR_PWD = "password1";
 
     public static final String STUDENT_URL = "http://onlinetestqand.rpcsys.hmco.com/StudentLogin.aspx";
+
+    public static final String STUDENT_ID = "";
+    public static final String SESSION_ID = "";
 }
 
 
