5101651ddf14a9ad3ea4e82ffc2c3ad21660a22e
123jayakrishna <jayakrishna.n@adeptpros.com>
2019-09-26 17:22:42 +0530

    With help of constants_26/09/2019

--- src/test/java/Pages/BaseClassForDM.java
+++ src/test/java/Pages/BaseClassForDM.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import utils.Constants;
+import utils.FIleLib;
 import utils.WebDriverCommonUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,7 @@ public class BaseClassForDM {
 
     public static WebDriver driver;
     public  WebDriverCommonUtils webDriverCommon;
-
+    public FIleLib fLib;
 
 
     public BaseClassForDM(WebDriver driver)
@@ -28,7 +29,9 @@ public class BaseClassForDM {
         driver= new ChromeDriver();
        driver.manage().window().maximize();
        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
+     //  driver.navigate().to("http://datamanagerqand.rpcsys.hmco.com/BalancedManagement/user/signin");
+       /* fLib= new FIleLib(driver);
+        driver.get(fLib.getValue("Proctor_url"));*/
        driver.get(Constants.PROCTOR_URL);
 
 
