4a079c565b0173151d6967c5e59484ba4e0fcfd3
123jayakrishna <jayakrishna.n@adeptpros.com>
2019-09-26 17:43:47 +0530

    With Proper allignment of Code.

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -6,7 +6,7 @@ public final class Constants {
     }
 
     public static final String PROCTOR_URL = "http://datamanagerqand.rpcsys.hmco.com/balancedmanagement/login.aspx";
-    public static final String PROCTOR_USERID = "rpcenviron+2017qa@gmail.com";
+    public static final String PROCTOR_USERID = "rpcdm2019+qauto@gmail.com";
     public static final String PROCTOR_PWD = "password1";
 
     public static final String STUDENT_URL = "http://onlinetestqand.rpcsys.hmco.com/StudentLogin.aspx";
