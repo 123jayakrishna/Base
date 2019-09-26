ee0094d0a0847f418a1bd65cab7ac6f5a252a74b
123jayakrishna <jayakrishna.n@adeptpros.com>
2019-09-26 17:26:15 +0530

    With help of constants_26/09/2019_17:26:04

--- src/test/java/Pages/BaseClassForDM.java
+++ src/test/java/Pages/BaseClassForDM.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import utils.Constants;
-import utils.FIleLib;
+
 import utils.WebDriverCommonUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -13,7 +13,7 @@ public class BaseClassForDM {
 
     public static WebDriver driver;
     public  WebDriverCommonUtils webDriverCommon;
-    public FIleLib fLib;
+
 
 
     public BaseClassForDM(WebDriver driver)
